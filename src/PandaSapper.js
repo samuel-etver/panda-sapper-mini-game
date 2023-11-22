0899ba9bc3c3c90886f53cdb1162a3215f76663a
Samuel Etver <samuel.etver@gmail.com>
2023-11-22 23:24:17 +0500

    Update PandaSapper.js

--- src/PandaSapper.js
+++ src/PandaSapper.js
@@ -36,6 +36,8 @@ var gridBombs;
 var gridType;
 var gridLevel;
 
+var proxyObj;
+
 var numberOfUnseen;
 var numberOfThink;
 
@@ -48,5 +50,145 @@ var tileStates = new Array(MAX_SIZE);
 (function() {
     for (var i = 0; i < MAX_SIZE; i++)
         tileStates[i] = new Array(MAX_SIZE);
-    tileStates[10][12] = 22;
 }());
+
+
+function hideBombs(xs, ys) {
+    var i
+    var x, y;
+
+   /* tileStates[xs][ys] += CORRECT;
+
+   *for(i = 0; i < gridBombs; i++) {
+        do {
+            x = (proxyObj.rand() >> 8) % gridWidth;
+            y = (proxyObj.rand() >> 8) % gridHeight;
+        } while(tileStates[x][y] !== UNSEEN || countAdjacent(x, y, CORRECT));
+        tileStates[x][y] += ACTUAL_BOMB;
+    }*/
+
+    tileStates[xs][ys] -= CORRECT;
+
+    for (x = 0; x < gridWidth; x++) {
+        for (y = 0; y < gridHeight; y++) {
+            if(!(tileStates[x][y] & ACTUAL_BOMB))
+                tileStates[x][y] += countAdjacent(x, y, ACTUAL_BOMB);
+        }
+    }
+
+    proxyObj.startClock(1);
+}
+
+
+function selectAdjacent(x, y) {
+    var dx, dy;
+    var dd = gridType === GAME_TRIANGLE ? 2 : 1;
+
+    if(tileStates[x][y] & UNSEEN || gameStatus === GAME_WAIT)
+        return;
+
+    var n = countAdjacent(x, y, THINK_BOMB);
+
+    if (tileStates[x][y] !== n)
+        return;
+
+    for(dx = -dd; dx <= dd; dx++) {
+        var tmpX = x + dx;
+        if(tmpX < 0 || tmpX >= gridWidth)
+            continue;
+
+        for(dy = -1; dy <= 1; dy++) {
+            var tmpY = y + dy;
+            if (tmpY < 0 || tmpY >= gridHeight)
+                continue;
+
+            if (dx === 0 && dy === 0)
+                continue;
+
+            if (gridType === GAME_HEXAGON && dy !== 0 && dx === (1 - 2 * (y % 2)))
+                continue;
+
+            if(gridType === GAME_TRIANGLE && dy === (2 * ((x + y) % 2) -1 ) && (dx === -2 || dx === 2))
+                continue;
+
+            if (!(tileStates[tmpX][tmpY] & THINK_BOMB))
+                proxyObj.selectSquare(tmpX, tmpY);
+        }
+    }
+}
+
+
+function removeEmpties(x, y) {
+    var dx, dy;
+    var dd = gridType === GAME_TRIANGLE ? 2 : 1;
+
+    if (!(tileStates[x][y] & UNSEEN) || (tileStates[x][y] & THINK_BOMB))
+       return;
+
+    tileStates[x][y] &= ~UNSEEN;
+
+    numberOfUnseen--;
+
+    proxyObj.drawSquare(x, y, tileStates[x][y]);
+
+    if(tileStates[x][y] !== EMPTY)
+        return;
+
+    for(dx = -dd; dx <= dd; dx++) {
+        var tmpX = x + dx;
+
+        if(tmpX < 0 || tmpX >= gridWidth)
+            continue;
+
+        for(dy = -1; dy <= 1; dy++) {
+            var tmpY = y + dy;
+
+            if(tmpY < 0 || tmpY >= gridHeight)
+                continue;
+
+            if(dx === 0 && dy === 0)
+                continue;
+
+            if(gridType === GAME_HEXAGON && dy !== 0 && dx === (1 - 2 * (y % 2)))
+                continue;
+
+            if(gridType === GAME_TRIANGLE && dy === (2 * ((x + y) % 2) - 1) && (dx === -2 || dx === 2))
+                continue;
+
+            removeEmpties(tmpX, tmpY);
+        }
+    }
+}
+
+
+function countAdjacent(x, y, flag) {
+    var n = 0;
+    var dx, dy;
+    var dd = (gridType === GAME_TRIANGLE) ? 2 : 1;
+
+    for (dx = -dd; dx <= dd; dx++) {
+        var tmpX = x + dx;
+        if (tmpX < 0 || tmpX >= gridWidth)
+            continue;
+
+        for (dy = -1; dy <= 1; dy++) {
+            var tmpY = y + dy;
+
+            if (tmpY < 0 || tmpY >= gridHeight)
+                continue;
+
+            if(gridType === GAME_HEXAGON && dy !== 0 && dx === (1 - 2 * (y % 2)))
+                continue;
+
+            if(gridType === GAME_TRIANGLE && dy === (2 * ((x + y) % 2) - 1) && (dx === -2 || dx === 2))
+                continue;
+
+            if(tileStates[tmpX][tmpY] & flag)
+                n++;
+        }
+    }
+
+    return n;
+}
+
+
